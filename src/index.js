7f9157693ca5fe8adbd6e5b81e248437cb41f9de
Insikynwa <pavel.churkin@mail.ru>
2021-03-16 01:32:51 +0300

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const bracketsArray = [];
+    bracketsConfig.forEach((item) => {
+        bracketsArray.push(item.join(""));
+    });
+
+    let count = 0;
+    const halfOfStrLength = str.length / 2;
+    do {
+        bracketsArray.forEach((item) => {
+            str = str.replace(item, "");
+        });
+        count++;
+    } while (count < halfOfStrLength && str.length > 0);
+
+    return str.length === 0 ? true : false;
+};
